5d2c992a26c83c6979398fcb0a5de2521b5644b9
Sebastián de la Fuente <sebadlf@gmail.com>
2019-08-21 22:24:03 -0300

    Render Reddit

--- src/components/Reddit.jsx
+++ src/components/Reddit.jsx
@@ -4,8 +4,18 @@ import "./Reddit.scss";
 
 const Reddit = ({ reddit }) => {
   const { data } = reddit;
-  const { title } = data;
-  return <div>{title}</div>;
+  const { author, title, thumbnail } = data;
+  return (
+    <div className="Reddit">
+      <div className="Reddit-Author">{author}</div>
+      {thumbnail ? (
+        <div className="Reddit-Image">
+          <img src={thumbnail} alt={title} />
+        </div>
+      ) : null}
+      <div className="Reddit-Title">{title}</div>
+    </div>
+  );
 };
 
 export default Reddit;
