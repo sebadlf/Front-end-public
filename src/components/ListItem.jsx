948e720b2c04bae537c10aa634351cfe8b95d706
Sebastián de la Fuente <sebadlf@gmail.com>
2019-08-21 23:11:07 -0300

    ListItem Styles

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -5,10 +5,34 @@ import "./ListItem.scss";
 
 const ListItem = ({ item }) => {
   const { data } = item;
-  const { id, title } = data;
+  const { id, title, thumbnail, author, created, readed, num_comments } = data;
+
+  const timeAgo = created;
   return (
     <div className="ListItem">
-      <Link to={`/reddit/${id}`}>{title} </Link>
+      <div className="ListItem-Header">
+        <div className="ListItem-Readed">
+          {!readed ? <span className="ListItem-ReadedMark" /> : null}
+        </div>
+        <div className="ListItem-Author">{author}</div>
+        <div className="ListItem-TimeAgo">{timeAgo}</div>
+      </div>
+      <Link className="ListItem-Link" to={`/reddit/${id}`}>
+        <div className="ListItem-Body">
+          {thumbnail ? (
+            <img className="ListItem-Image" src={thumbnail} alt={title} />
+          ) : null}
+          <div className="ListItem-Title">{title}</div>
+          <div className="ListItem-Arrow">></div>
+        </div>
+      </Link>
+      <div className="ListItem-Footer">
+        <div className="ListItem-Dismiss">
+          <span className="ListItem-DismissSymbol">X</span>
+          Dismiss
+        </div>
+        <div className="ListItem-NumComments">{num_comments} Comments</div>
+      </div>
     </div>
   );
 };
